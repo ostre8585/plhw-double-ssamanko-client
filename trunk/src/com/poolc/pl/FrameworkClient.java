7adc7f380bda4031185c84594305b56cbe402eab
yoite@lycos.co.kr <yoite@lycos.co.kr@2b0e4492-312a-0947-4c2e-5c4e7d073cce>
2011-05-29 14:49:43 +0000

    modify

--- trunk/src/com/poolc/pl/FrameworkClient.java
+++ trunk/src/com/poolc/pl/FrameworkClient.java
@@ -11,49 +11,39 @@ import java.net.UnknownHostException;
 
 
 public class FrameworkClient {
-	private final static FrameworkClient instance = new FrameworkClient();
-	private BufferedReader br;
-	private PrintWriter pw;
-	
-	private Socket sock;
-	
-	public FrameworkClient() {
+	private static Socket sock;
+	private static BufferedReader br;
+	private static PrintWriter pw;
+	public static void main(String[] args) {
 		try {
-		
 			sock = new Socket("127.0.0.1", 10002);
 			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
 			pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
-			
+
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				pw.close();
+				sock.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
-	}
-	
-	public static FrameworkClient getInstance() {
-		return instance;
-	}
-	
-	public void run() {
-		//do something
+				
 		
 		
-		
-		
-		
-		//when sock close 
-//		try {
-//			br.close();
-//			pw.close();
-//			sock.close();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	}
 
+	}
+	
+	
 
 }
